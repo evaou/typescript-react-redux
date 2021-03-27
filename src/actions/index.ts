ed3940b0fb562483a98f2147b6f84c677b4fe402
evaou <speeder528@gmail.com>
2021-03-27 20:44:57 +0800

    add action interface

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,19 +2,24 @@ import axios from 'axios';
 import { Dispatch } from 'redux';
 import { ActionTypes } from './types';
 
-interface ToDo {
+interface Todo {
   id: number;
   title: string;
   completed: boolean;
+}
+
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
 }
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get<ToDo[]>(url);
+    const response = await axios.get<Todo[]>(url);
 
-    dispatch({
+    dispatch<FetchTodosAction>({
       type: ActionTypes.fetchTodos,
       payload: response.data,
     });
